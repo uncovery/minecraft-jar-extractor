65e3f347f0bc479f93009b12674305214bc3427f
Uncovery <minecraft@uncovery.me>
2020-08-31 14:20:16 +0800

    Add changes from upstream

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # minecraft-jar-extractor
+
 [![NPM version](https://img.shields.io/npm/v/minecraft-jar-extractor.svg)](http://npmjs.com/package/minecraft-jar-extractor)
-[![Build Status](https://circleci.com/gh/PrismarineJS/minecraft-jar-extractor/tree/master.svg?style=shield)](https://circleci.com/gh/PrismarineJS/minecraft-jar-extractor/tree/master)
+[![Build Status](https://github.com/PrismarineJS/minecraft-jar-extractor/workflows/CI/badge.svg)](https://github.com/PrismarineJS/minecraft-jar-extractor/actions?query=workflow%3A%22CI%22)
+[![Discord](https://img.shields.io/badge/chat-on%20discord-brightgreen.svg)](https://discord.gg/GsEFRM8)
+[![Gitter](https://img.shields.io/badge/chat-on%20gitter-brightgreen.svg)](https://gitter.im/PrismarineJS/general)
+[![Irc](https://img.shields.io/badge/chat-on%20irc-brightgreen.svg)](https://irc.gitter.im/)
+
+[![Try it on gitpod](https://img.shields.io/badge/try-on%20gitpod-brightgreen.svg)](https://gitpod.io/#https://github.com/PrismarineJS/minecraft-jar-extractor)
 
 Extract structured data from the minecraft jar
 
 ## image name extractor
 
-Just run `node image_names.js <version1,version2,...> <output_dir> <temporary_dir>` 
+Just run `node image_names.js <version1,version2,...> <output_dir> <temporary_dir>`
 and get a directory containing the name->texture mapping and the textures.
 
 Result of that currently online at [minecraft-assets](https://github.com/rom1504/minecraft-assets)
 
 ## lang extractor
 
-Just run `node lang.js <version1,version2,...> <output_dir> <temporary_dir>` 
+Just run `node lang.js <version1,version2,...> <output_dir> <temporary_dir>`
 and get a directory containing en_us.lang and en_us.json
 
 ## protocol extractor
@@ -24,8 +30,18 @@ How to use it:
 2. `node protocol_extractor.js /tmp/the_output/decompiled`
 3. get a protocol.json describing the classname<->id relation and some fields information
 
+## block loot table extractor
+
+Only works in 1.14+ as the loot table files did not exist before this.
+
+Make sure to first extract the data folder from Minecraft jar if not done already using:
+`node extract_datafolder.js <version1,version2,...> <output_dir> <temporary_dir>`
+
+Next you can extract the actual loot table data to Minecraft-Data using:
+`node extract_lootTables.js <version1,version2,...> <extractedDataFolder> <mcDataFolder>`
+
 ## History
 
 ### 0.0.0
 
-* imported from minecraft-data + description added
+- imported from minecraft-data + description added
--- image_names.js
+++ image_names.js
@@ -26,194 +26,196 @@ minecraftVersions.forEach(minecraftVersion => {
 // doesn't support 1.7.10 : completely different organization (no block state, no model)
 const itemMapping = {
   '1.8.8': {
-    'wooden_door': 'oak_door',
-    'fish': 'pufferfish',
-    'cooked_fish': 'cooked_salmon',
-    'dye': 'dye_black',
-    'potion': 'bottle_drinkable',
-    'skull': 'skull_char'
+    wooden_door: 'oak_door',
+    fish: 'pufferfish',
+    cooked_fish: 'cooked_salmon',
+    dye: 'dye_black',
+    potion: 'bottle_drinkable',
+    skull: 'skull_char'
   },
-  '1.9': {
-    'wooden_door': 'oak_door',
-    'fish': 'pufferfish',
-    'cooked_fish': 'cooked_salmon',
-    'dye': 'dye_black',
-    'potion': 'bottle_drinkable',
-    'skull': 'skull_char',
-    'boat': 'oak_boat',
-    'splash_potion': 'bottle_splash',
-    'lingering_potion': 'bottle_lingering'
+  1.9: {
+    wooden_door: 'oak_door',
+    fish: 'pufferfish',
+    cooked_fish: 'cooked_salmon',
+    dye: 'dye_black',
+    potion: 'bottle_drinkable',
+    skull: 'skull_char',
+    boat: 'oak_boat',
+    splash_potion: 'bottle_splash',
+    lingering_potion: 'bottle_lingering'
   },
   '1.10': {
-    'wooden_door': 'oak_door',
-    'fish': 'pufferfish',
-    'cooked_fish': 'cooked_salmon',
-    'dye': 'dye_black',
-    'potion': 'bottle_drinkable',
-    'skull': 'skull_char',
-    'boat': 'oak_boat',
-    'splash_potion': 'bottle_splash',
-    'lingering_potion': 'bottle_lingering'
+    wooden_door: 'oak_door',
+    fish: 'pufferfish',
+    cooked_fish: 'cooked_salmon',
+    dye: 'dye_black',
+    potion: 'bottle_drinkable',
+    skull: 'skull_char',
+    boat: 'oak_boat',
+    splash_potion: 'bottle_splash',
+    lingering_potion: 'bottle_lingering'
   },
   '1.11.2': {
-    'wooden_door': 'oak_door',
-    'fish': 'pufferfish',
-    'cooked_fish': 'cooked_salmon',
-    'dye': 'dye_black',
-    'potion': 'bottle_drinkable',
-    'skull': 'skull_char',
-    'boat': 'oak_boat',
-    'totem_of_undying': 'totem',
-    'splash_potion': 'bottle_splash',
-    'lingering_potion': 'bottle_lingering'
+    wooden_door: 'oak_door',
+    fish: 'pufferfish',
+    cooked_fish: 'cooked_salmon',
+    dye: 'dye_black',
+    potion: 'bottle_drinkable',
+    skull: 'skull_char',
+    boat: 'oak_boat',
+    totem_of_undying: 'totem',
+    splash_potion: 'bottle_splash',
+    lingering_potion: 'bottle_lingering'
   },
-  '1.12': {
-    'wooden_door': 'oak_door',
-    'fish': 'pufferfish',
-    'cooked_fish': 'cooked_salmon',
-    'dye': 'dye_black',
-    'potion': 'bottle_drinkable',
-    'skull': 'skull_char',
-    'boat': 'oak_boat',
-    'totem_of_undying': 'totem',
-    'splash_potion': 'bottle_splash',
-    'lingering_potion': 'bottle_lingering'
+  1.12: {
+    wooden_door: 'oak_door',
+    fish: 'pufferfish',
+    cooked_fish: 'cooked_salmon',
+    dye: 'dye_black',
+    potion: 'bottle_drinkable',
+    skull: 'skull_char',
+    boat: 'oak_boat',
+    totem_of_undying: 'totem',
+    splash_potion: 'bottle_splash',
+    lingering_potion: 'bottle_lingering'
   },
-  '1.13': {
+  1.13: {
   }
 }
 
 // TODO: read the decompiled code (automatically) to generate this
 const blockMapping = {
   '1.8.8': {
-    'log': 'oak_log',
-    'planks': 'oak_planks',
-    'sapling': 'oak_sapling',
-    'leaves': 'oak_leaves',
-    'tallgrass': 'tall_grass',
-    'deadbush': 'dead_bush',
-    'wool': 'white_wool',
-    'piston_extension': 'piston',
-    'yellow_flower': 'dandelion',
-    'red_flower': 'poppy',
-    'double_stone_slab': 'stone_double_slab',
-    'stained_glass': 'white_stained_glass',
-    'double_wooden_slab': 'oak_double_slab',
-    'wooden_slab': 'oak_slab',
-    'stained_hardened_clay': 'white_stained_hardened_clay',
-    'stained_glass_pane': 'white_stained_glass_pane',
-    'log2': 'acacia_log',
-    'leaves2': 'acacia_leaves',
-    'carpet': 'white_carpet',
-    'double_plant': 'sunflower',
-    'double_stone_slab2': 'red_sandstone_double_slab',
-    'stone_slab2': 'red_sandstone_slab'
+    log: 'oak_log',
+    planks: 'oak_planks',
+    sapling: 'oak_sapling',
+    leaves: 'oak_leaves',
+    tallgrass: 'tall_grass',
+    deadbush: 'dead_bush',
+    wool: 'white_wool',
+    piston_extension: 'piston',
+    yellow_flower: 'dandelion',
+    red_flower: 'poppy',
+    double_stone_slab: 'stone_double_slab',
+    stained_glass: 'white_stained_glass',
+    double_wooden_slab: 'oak_double_slab',
+    wooden_slab: 'oak_slab',
+    stained_hardened_clay: 'white_stained_hardened_clay',
+    stained_glass_pane: 'white_stained_glass_pane',
+    log2: 'acacia_log',
+    leaves2: 'acacia_leaves',
+    carpet: 'white_carpet',
+    double_plant: 'sunflower',
+    double_stone_slab2: 'red_sandstone_double_slab',
+    stone_slab2: 'red_sandstone_slab'
   },
-  '1.9': {
-    'log': 'oak_log',
-    'planks': 'oak_planks',
-    'sapling': 'oak_sapling',
-    'leaves': 'oak_leaves',
-    'tallgrass': 'tall_grass',
-    'deadbush': 'dead_bush',
-    'wool': 'white_wool',
-    'piston_extension': 'piston',
-    'yellow_flower': 'dandelion',
-    'red_flower': 'poppy',
-    'double_stone_slab': 'stone_double_slab',
-    'stained_glass': 'white_stained_glass',
-    'double_wooden_slab': 'oak_double_slab',
-    'wooden_slab': 'oak_slab',
-    'stained_hardened_clay': 'white_stained_hardened_clay',
-    'stained_glass_pane': 'white_stained_glass_pane',
-    'log2': 'acacia_log',
-    'leaves2': 'acacia_leaves',
-    'carpet': 'white_carpet',
-    'double_plant': 'sunflower',
-    'double_stone_slab2': 'red_sandstone_double_slab',
-    'stone_slab2': 'red_sandstone_slab'
+  1.9: {
+    log: 'oak_log',
+    planks: 'oak_planks',
+    sapling: 'oak_sapling',
+    leaves: 'oak_leaves',
+    tallgrass: 'tall_grass',
+    deadbush: 'dead_bush',
+    wool: 'white_wool',
+    piston_extension: 'piston',
+    yellow_flower: 'dandelion',
+    red_flower: 'poppy',
+    double_stone_slab: 'stone_double_slab',
+    stained_glass: 'white_stained_glass',
+    double_wooden_slab: 'oak_double_slab',
+    wooden_slab: 'oak_slab',
+    stained_hardened_clay: 'white_stained_hardened_clay',
+    stained_glass_pane: 'white_stained_glass_pane',
+    log2: 'acacia_log',
+    leaves2: 'acacia_leaves',
+    carpet: 'white_carpet',
+    double_plant: 'sunflower',
+    double_stone_slab2: 'red_sandstone_double_slab',
+    stone_slab2: 'red_sandstone_slab'
   },
   '1.10': {
-    'log': 'oak_log',
-    'planks': 'oak_planks',
-    'sapling': 'oak_sapling',
-    'leaves': 'oak_leaves',
-    'tallgrass': 'tall_grass',
-    'deadbush': 'dead_bush',
-    'wool': 'white_wool',
-    'piston_extension': 'piston',
-    'yellow_flower': 'dandelion',
-    'red_flower': 'poppy',
-    'double_stone_slab': 'stone_double_slab',
-    'stained_glass': 'white_stained_glass',
-    'double_wooden_slab': 'oak_double_slab',
-    'wooden_slab': 'oak_slab',
-    'stained_hardened_clay': 'white_stained_hardened_clay',
-    'stained_glass_pane': 'white_stained_glass_pane',
-    'log2': 'acacia_log',
-    'leaves2': 'acacia_leaves',
-    'carpet': 'white_carpet',
-    'double_plant': 'sunflower',
-    'double_stone_slab2': 'red_sandstone_double_slab',
-    'stone_slab2': 'red_sandstone_slab'
+    log: 'oak_log',
+    planks: 'oak_planks',
+    sapling: 'oak_sapling',
+    leaves: 'oak_leaves',
+    tallgrass: 'tall_grass',
+    deadbush: 'dead_bush',
+    wool: 'white_wool',
+    piston_extension: 'piston',
+    yellow_flower: 'dandelion',
+    red_flower: 'poppy',
+    double_stone_slab: 'stone_double_slab',
+    stained_glass: 'white_stained_glass',
+    double_wooden_slab: 'oak_double_slab',
+    wooden_slab: 'oak_slab',
+    stained_hardened_clay: 'white_stained_hardened_clay',
+    stained_glass_pane: 'white_stained_glass_pane',
+    log2: 'acacia_log',
+    leaves2: 'acacia_leaves',
+    carpet: 'white_carpet',
+    double_plant: 'sunflower',
+    double_stone_slab2: 'red_sandstone_double_slab',
+    stone_slab2: 'red_sandstone_slab'
   },
   '1.11.2': {
-    'log': 'oak_log',
-    'planks': 'oak_planks',
-    'sapling': 'oak_sapling',
-    'leaves': 'oak_leaves',
-    'tallgrass': 'tall_grass',
-    'deadbush': 'dead_bush',
-    'wool': 'white_wool',
-    'piston_extension': 'piston',
-    'yellow_flower': 'dandelion',
-    'red_flower': 'poppy',
-    'double_stone_slab': 'stone_double_slab',
-    'stained_glass': 'white_stained_glass',
-    'double_wooden_slab': 'oak_double_slab',
-    'wooden_slab': 'oak_slab',
-    'stained_hardened_clay': 'white_stained_hardened_clay',
-    'stained_glass_pane': 'white_stained_glass_pane',
-    'log2': 'acacia_log',
-    'leaves2': 'acacia_leaves',
-    'carpet': 'white_carpet',
-    'double_plant': 'sunflower',
-    'double_stone_slab2': 'red_sandstone_double_slab',
-    'stone_slab2': 'red_sandstone_slab'
+    log: 'oak_log',
+    planks: 'oak_planks',
+    sapling: 'oak_sapling',
+    leaves: 'oak_leaves',
+    tallgrass: 'tall_grass',
+    deadbush: 'dead_bush',
+    wool: 'white_wool',
+    piston_extension: 'piston',
+    yellow_flower: 'dandelion',
+    red_flower: 'poppy',
+    double_stone_slab: 'stone_double_slab',
+    stained_glass: 'white_stained_glass',
+    double_wooden_slab: 'oak_double_slab',
+    wooden_slab: 'oak_slab',
+    stained_hardened_clay: 'white_stained_hardened_clay',
+    stained_glass_pane: 'white_stained_glass_pane',
+    log2: 'acacia_log',
+    leaves2: 'acacia_leaves',
+    carpet: 'white_carpet',
+    double_plant: 'sunflower',
+    double_stone_slab2: 'red_sandstone_double_slab',
+    stone_slab2: 'red_sandstone_slab'
   },
-  '1.12': {
-    'log': 'oak_log',
-    'planks': 'oak_planks',
-    'sapling': 'oak_sapling',
-    'leaves': 'oak_leaves',
-    'tallgrass': 'tall_grass',
-    'deadbush': 'dead_bush',
-    'wool': 'white_wool',
-    'piston_extension': 'piston',
-    'yellow_flower': 'dandelion',
-    'red_flower': 'poppy',
-    'double_stone_slab': 'stone_double_slab',
-    'stained_glass': 'white_stained_glass',
-    'double_wooden_slab': 'oak_double_slab',
-    'wooden_slab': 'oak_slab',
-    'stained_hardened_clay': 'white_stained_hardened_clay',
-    'stained_glass_pane': 'white_stained_glass_pane',
-    'log2': 'acacia_log',
-    'leaves2': 'acacia_leaves',
-    'carpet': 'white_carpet',
-    'double_plant': 'sunflower',
-    'double_stone_slab2': 'red_sandstone_double_slab',
-    'stone_slab2': 'red_sandstone_slab'
+  1.12: {
+    log: 'oak_log',
+    planks: 'oak_planks',
+    sapling: 'oak_sapling',
+    leaves: 'oak_leaves',
+    tallgrass: 'tall_grass',
+    deadbush: 'dead_bush',
+    wool: 'white_wool',
+    piston_extension: 'piston',
+    yellow_flower: 'dandelion',
+    red_flower: 'poppy',
+    double_stone_slab: 'stone_double_slab',
+    stained_glass: 'white_stained_glass',
+    double_wooden_slab: 'oak_double_slab',
+    wooden_slab: 'oak_slab',
+    stained_hardened_clay: 'white_stained_hardened_clay',
+    stained_glass_pane: 'white_stained_glass_pane',
+    log2: 'acacia_log',
+    leaves2: 'acacia_leaves',
+    carpet: 'white_carpet',
+    double_plant: 'sunflower',
+    double_stone_slab2: 'red_sandstone_double_slab',
+    stone_slab2: 'red_sandstone_slab'
   },
-  '1.13': {
+  1.13: {
   }
 }
 
-function extractBlockState (name, path) {
+function extractBlockState (name, path, full = false) {
   if (name === null) { return null } else {
     try {
+      name = name.replace(/minecraft:/, '')
       const t = JSON.parse(fs.readFileSync(path + name + '.json', 'utf8'))
-      const firstVariant = t['variants'][Object.keys(t['variants'])[0]]
+      if (full) return t
+      const firstVariant = t.variants[Object.keys(t.variants)[0]]
       return firstVariant.model || firstVariant[0].model
     } catch (err) {
       return null
@@ -221,12 +223,14 @@ function extractBlockState (name, path) {
   }
 }
 
-function extractModel (name, path) {
+function extractModel (name, path, full = false) {
   if (name === null) {
     return null
   } else {
     try {
+      name = name.replace(/^(?:block\/)?minecraft:/, '')
       const t = JSON.parse(fs.readFileSync(path + name + '.json', 'utf8'))
+      if (full) return t
       if (t.textures) {
         return t.textures[Object.keys(t.textures)[0]]
       }
@@ -247,12 +251,12 @@ function extractModel (name, path) {
 function getItems (unzippedFilesDir, itemsTexturesPath, itemMapping, version) {
   const mcData = require('minecraft-data')(version)
   const itemTextures = mcData.itemsArray.map(item => {
-    const model = itemMapping !== undefined && itemMapping[item.name] ? itemMapping[item.name] : item.name
+    const model = (itemMapping !== undefined && itemMapping[item.name] ? itemMapping[item.name] : item.name).replace(/minecraft:/, '')
     const texture = extractModel('item/' + model, unzippedFilesDir + '/assets/minecraft/models/')
     return {
       name: item.name,
-      model: model === null ? null : model.replace('item/', 'items/'),
-      texture: texture === null ? null : texture.replace('item/', 'items/')
+      model: !model ? null : model.replace('item/', 'items/'),
+      texture: !texture ? null : texture.replace('item/', 'items/')
     }
   })
   fs.writeFileSync(itemsTexturesPath, JSON.stringify(itemTextures, null, 2))
@@ -261,17 +265,36 @@ function getItems (unzippedFilesDir, itemsTexturesPath, itemMapping, version) {
 function getBlocks (unzippedFilesDir, blocksTexturesPath, blockMapping, version) {
   const mcData = require('minecraft-data')(version)
   const blockModel = mcData.blocksArray.map(block => {
-    const blockState = blockMapping !== undefined && blockMapping[block.name] ? blockMapping[block.name] : block.name
+    const blockState = (blockMapping !== undefined && blockMapping[block.name] ? blockMapping[block.name] : block.name).replace(/minecraft:/, '')
     const model = extractBlockState(blockState, unzippedFilesDir + '/assets/minecraft/blockstates/')
-    const texture = extractModel(model === null ? null : (model.startsWith('block/') ? model : 'block/' + model), unzippedFilesDir + '/assets/minecraft/models/')
+    const texture = extractModel(!model ? null : (model.startsWith('block/') ? model : 'block/' + model), unzippedFilesDir + '/assets/minecraft/models/')
     return {
       name: block.name,
       blockState: blockState,
-      model: model === null ? null : model.replace('block/', 'blocks/'),
-      texture: texture === null ? null : texture.replace('block/', 'blocks/')
+      model: !model ? null : model.replace('block/', 'blocks/'),
+      texture: !texture ? null : texture.replace('block/', 'blocks/')
     }
   })
   fs.writeFileSync(blocksTexturesPath, JSON.stringify(blockModel, null, 2))
+}
+
+function getModels (unzippedFilesDir, blocksStatesPath, blocksModelsPath, blockMapping, version) {
+  const mcData = require('minecraft-data')(version)
+  const blocksStates = {}
+  for (const block of mcData.blocksArray) {
+    const blockState = blockMapping !== undefined && blockMapping[block.name] ? blockMapping[block.name] : block.name
+    const state = extractBlockState(blockState, unzippedFilesDir + '/assets/minecraft/blockstates/', true)
+    blocksStates[block.name] = state
+  }
+  const modelsPath = unzippedFilesDir + '/assets/minecraft/models/block/'
+  const modelFiles = fs.readdirSync(modelsPath)
+  const models = {}
+  for (const name of modelFiles) {
+    const model = require(modelsPath + name)
+    models[name.split('.')[0]] = model
+  }
+  fs.writeFileSync(blocksStatesPath, JSON.stringify(blocksStates, null, 2))
+  fs.writeFileSync(blocksModelsPath, JSON.stringify(models, null, 2))
 }
 
 function copyTextures (unzippedFilesDir, outputDir, minecraftVersion) {
@@ -302,7 +325,7 @@ function generateTextureContent (outputDir) {
   const arr = blocksItems.map(b => ({
     name: b.name,
     texture: b.texture == null ? null
-      : ('data:image/png;base64,' + fs.readFileSync(outputDir + '/' + b.texture.replace('item/', 'items/').replace('block/', 'blocks/') + '.png', 'base64'))
+      : ('data:image/png;base64,' + fs.readFileSync(outputDir + '/' + b.texture.replace('item/', 'items/').replace('block/', 'blocks/').replace(/minecraft:/, '') + '.png', 'base64'))
   }))
   fs.writeFileSync(outputDir + '/texture_content.json', JSON.stringify(arr, null, 2))
 }
@@ -316,6 +339,7 @@ function extract (minecraftVersion, outputDir, temporaryDir, cb) {
     fs.mkdirpSync(outputDir)
     getItems(unzippedFilesDir, outputDir + '/items_textures.json', itemMapping[minecraftVersion], minecraftVersion)
     getBlocks(unzippedFilesDir, outputDir + '/blocks_textures.json', blockMapping[minecraftVersion], minecraftVersion)
+    getModels(unzippedFilesDir, outputDir + '/blocks_states.json', outputDir + '/blocks_models.json', blockMapping[minecraftVersion], minecraftVersion)
     copyTextures(unzippedFilesDir, outputDir, minecraftVersion)
     generateTextureContent(outputDir)
     cb()
